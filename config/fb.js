c254a4480a9d750d42cc91cc08b4c1178a4fe8fb
rowmendza <rowmendz@mail.sfsu.edu>
2015-12-11 18:11:36 +0000

    initial commit

---
+++ config/fb.js
@@ -0,0 +1,6 @@
+// facebook app settings - fb.js
+module.exports = {
+  'appID' : '470559133129661',
+  'appSecret' : '49738c2de4609a522c05ff47882d4cae',
+  'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+};
